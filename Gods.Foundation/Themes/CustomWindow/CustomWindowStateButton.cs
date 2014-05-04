0b5d69b5b3db414eb517498cfcfe47f1ceaaeafe
ljsunlin <ljsunlin@126.com>
2014-05-04 21:57:33 +0800

    [Feature] 启动AntWpfDemos项目，Gods.Foundation程序集准备完毕，Demo1开发进行中

---
+++ Gods.Foundation/Themes/CustomWindow/CustomWindowStateButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Gods.Foundation
+{
+    public class CustomWindowStateButton : Button
+    {
+        static CustomWindowStateButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof (CustomWindowStateButton),
+                                                     new FrameworkPropertyMetadata(typeof (CustomWindowStateButton)));
+        }
+
+        public Brush BackgroundHoverBrush
+        {
+            get { return (Brush)GetValue(BackgroundHoverBrushProperty); }
+            set { SetValue(BackgroundHoverBrushProperty, value); }
+        }
+
+        public static readonly DependencyProperty BackgroundHoverBrushProperty =
+            DependencyProperty.Register("BackgroundHoverBrush", typeof(Brush), typeof(CustomWindowStateButton));
+
+
+
+
+        public ImageSource ImageSource
+        {
+            get { return (ImageSource)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(CustomWindowStateButton));
+
+        
+    }
+}
