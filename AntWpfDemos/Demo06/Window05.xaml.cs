6903434200ca53c5dbae7d7184e34aedd3b7addc
ljsunlin <ljsunlin@126.com>
2014-05-15 00:01:25 +0800

    [Feature] Demo06增加高级动画效果，有趣的圆圈

---
+++ AntWpfDemos/Demo06/Window05.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Demo06
+{
+    /// <summary>
+    /// Window05.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window05 : Window
+    {
+        public Window05()
+        {
+            InitializeComponent();
+        }
+
+        private void Window05_OnLoaded(object sender, RoutedEventArgs e)
+        {
+//            ListBoxItem item = new ListBoxItem()
+//                {
+//                    Content = "123"
+//                };
+//
+//            canvas.Children.Add(item);
+
+//            DispatcherTimer timer = new DispatcherTimer(
+//                TimeSpan.FromSeconds(0.3),
+//                DispatcherPriority.Normal,
+//                (o, args) =>
+//                    {
+//                        funnyItemsPanel.Children.Add(new FunnyItemControl());
+//                    },
+//                this);
+
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = TimeSpan.FromSeconds(0.1);
+            timer.Start();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            funnyItemsPanel.Children.Add(new FunnyItemControl());
+        }
+    }
+}
---
+++ AntWpfDemos/Demo06/Window05/FunnyItemsPanel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Demo06
+{
+    class FunnyItemsPanel : Canvas
+    {
+        private const double LARGE_CIRCLE_RADIUS = 300;
+        private const int ROUND_ITEMSCOUNT = 24;
+        private const double UNIT_ANGLE = 360.0/ROUND_ITEMSCOUNT;
+        private double _itemRadius;
+        private double _itemWidth;
+        private double _itemDistance;  // 主圆心到item圆心的长度
+        private double _currentMainCircleRadius;
+        private double _currentAngle = 270;
+        private int _itemsCount = 0;
+
+        private static double Sin(double angle)
+        {
+            return Math.Sin(angle/180*Math.PI);
+        }
+
+        private static double Cos(double angle)
+        {
+            return Math.Cos(angle / 180 * Math.PI);
+        }
+
+        public FunnyItemsPanel()
+        {
+            _currentMainCircleRadius = LARGE_CIRCLE_RADIUS;
+            Caculate();
+        }
+
+        private void Caculate()
+        {
+            // 小圆半径 = 大圆半径 * cos(a)
+            _itemRadius = _currentMainCircleRadius * Sin(UNIT_ANGLE / 2);
+            _itemWidth = _itemRadius * 2;
+            _itemDistance = _currentMainCircleRadius * Cos(UNIT_ANGLE / 2);
+        }
+
+        protected override void OnVisualChildrenChanged(DependencyObject visualAdded, DependencyObject visualRemoved)
+        {
+            var uiElement = visualAdded as UIElement;
+
+            uiElement.SetValue(FrameworkElement.WidthProperty, _itemWidth);
+            uiElement.SetValue(FrameworkElement.HeightProperty, _itemWidth);
+
+
+            double itemCenterX = _itemDistance*Cos(_currentAngle) + _currentMainCircleRadius;
+            double itemCenterY = _itemDistance*Sin(_currentAngle) + _currentMainCircleRadius;
+
+            double itemLeft = itemCenterX - _itemRadius;
+            double itemTop = itemCenterY - _itemRadius;
+
+            DoubleAnimation xLocAnimation = new DoubleAnimation()
+                {
+                    From = _currentMainCircleRadius - _itemRadius,
+                    To = itemLeft,
+                    Duration = TimeSpan.FromSeconds(0.3),
+                    AccelerationRatio = 1, // 始终加速
+                };
+
+            DoubleAnimation yLocAnimation = new DoubleAnimation()
+                {
+                    From = _currentMainCircleRadius - _itemRadius,
+                    To = itemTop,
+                    Duration = TimeSpan.FromSeconds(0.3),
+                    AccelerationRatio = 1,
+                };
+
+            uiElement.BeginAnimation(Canvas.LeftProperty, xLocAnimation);
+            uiElement.BeginAnimation(Canvas.TopProperty, yLocAnimation);
+
+//            SetLeft(uiElement, itemCenterX);
+//            SetTop(uiElement, itemCenterY);
+
+            base.OnVisualChildrenChanged(visualAdded, visualRemoved);
+
+            _currentAngle -= UNIT_ANGLE;
+            ++_itemsCount;
+
+            if (_itemsCount % ROUND_ITEMSCOUNT == 0)
+            {
+                _currentMainCircleRadius -= _itemRadius;
+                Caculate();
+            }
+        }
+    }
+}
